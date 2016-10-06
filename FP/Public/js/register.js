7025a62dc1b9a895bb4e7bce0d4096d1e5a01448
JunxLau <junx_lau@hotmail.com>
2016-10-06 22:37:04 +0800

    a

--- FP/Public/js/register.js
+++ FP/Public/js/register.js
@@ -2,6 +2,9 @@
 			function emailFunction() {
     			if (document.getElementById("email").value == "") {
 				$("#errormsg_email").text("You can't leave this empty.");
+				}
+				else{
+					$("#errormsg_email").text("");
 				}	
 			}
 			
@@ -27,6 +30,9 @@
     			if (document.getElementById("password").value == "") {
 					$("#errormsg_password").text("You can't leave this empty.");
 				}
+				else if(document.getElementById("password").value.length < 8 || document.getElementById("password").value.length > 16){
+					$("#errormsg_password").text("Password length must be between 8 to 6 characters.");
+				}
 				else{
 					$("#errormsg_password").text("");
 				}
@@ -36,12 +42,11 @@
 				if (document.getElementById("confirmPassword").value == "") {
 					$("#errormsg_confirmPassword").text("You can't leave this empty.");
 				}
+				else if (document.getElementById("confirmPassword").value != document.getElementById("password").value) {
+					$("#errormsg_confirmPassword").text("These passwords don't match. Try again?");
+				}
 				else{
 					$("#errormsg_confirmPassword").text("");
-				}
-				
-				if (document.getElementById("confirmPassword").value != document.getElementById("password").value) {
-					$("#errormsg_confirmPassword").text("These passwords don't match. Try again?");
 				}
 			}
 			
@@ -71,24 +76,75 @@
 			$('#registerForm').submit(function() {
 			var a = true;
 			
+			//Email
+				if (document.getElementById("email").value == "") {
+				$("#errormsg_email").text("You can't leave this empty.");
+				}
+				else{
+					$("#errormsg_email").text("");
+				}
+				
+			
 			//FirstName
     			if (document.getElementById("firstName").value == "") {
-				document.getElementById("errormsg_firstName").style.display = "block";
+				$("#errormsg_firstName").text("You can't leave this empty.");
 				a=false;
 				}
 				else{
-					document.getElementById("errormsg_firstName").style.display = "none";
-				}
+					$("#errormsg_firstName").text("");
+				}	
 				
 			//LastName			
     			if (document.getElementById("lastName").value == "") {
-				document.getElementById("errormsg_lastName").style.display = "block";
+				$("#errormsg_lastName").text("You can't leave this empty.");
 				a=false;
 				}
 				else{
-					document.getElementById("errormsg_lastName").style.display = "none";
+					$("#errormsg_lastName").text("");
 				}
 				
+			//Password
+				if (document.getElementById("password").value == "") {
+					$("#errormsg_password").text("You can't leave this empty.");
+					a=false;
+				}
+				else if(document.getElementById("password").value.length < 8 || document.getElementById("password").value.length > 16){
+					$("#errormsg_password").text("Password length must be between 8 to 6 characters.");
+					a=false;
+				}
+				else{
+					$("#errormsg_password").text("");
+				}
+				
+			//ConfirmPassword
+				if (document.getElementById("confirmPassword").value == "") {
+					$("#errormsg_confirmPassword").text("You can't leave this empty.");
+					a=false;
+				}
+				else if (document.getElementById("confirmPassword").value != document.getElementById("password").value) {
+					$("#errormsg_confirmPassword").text("These passwords don't match. Try again?");
+					a=false;
+				}
+				else{
+					$("#errormsg_confirmPassword").text("");
+				}
+				
+			//DOB
+				if (document.getElementById("datepicker").value == "") {
+				$("#errormsg_DOB").text("Please select your date of birth.");
+				a=false;
+				}
+				else{
+					$("#errormsg_DOB").text("");
+				}
+				
+			//Occupation
+				if (document.getElementById("occupation").value == "") {
+				$("#errormsg_occupation").text("You can't leave this empty.");
+				}
+				else{
+					$("#errormsg_occupation").text("");
+				}
 				
     			if (a){
     			return true; 
