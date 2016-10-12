33dd68516364cc7b3d3d2e9d78462c1108034ac3
a70537952 <a70537952@gmail.com>
2016-10-12 22:55:55 +0800

    a

--- FP/Public/js/overviewDemo.js
+++ FP/Public/js/overviewDemo.js
@@ -6,11 +6,12 @@ var isNavOpen = false;
 function setNav() {
     if (isNavOpen) {
         document.getElementById("profile").style.marginRight = "0px";
+
         document.getElementById("navControl").innerHTML = "&#9776";
         document.getElementById("userLogo").style.display = "none"
         document.getElementById("mySidenav").style.width = "50px";
         document.getElementsByClassName("main")[0].style.marginLeft = "0px";
-        document.getElementsByClassName("main")[1].style.marginLeft = "50px";
+        document.getElementsByClassName("main")[1].style.marginLeft = "0px";
         isNavOpen = false;
     } else {
         document.getElementById("profile").style.marginRight = "250px";
@@ -19,6 +20,7 @@ function setNav() {
         document.getElementById("mySidenav").style.width = "250px";
         document.getElementsByClassName("main")[0].style.marginLeft = "250px";
         document.getElementsByClassName("main")[1].style.marginLeft = "250px";
+
         isNavOpen = true;
     }
 
